1847ec2ea097bd7fc01aaed4b502095d9bc16bb9
dark-r00t <79925808+dark-r00t@users.noreply.github.com>
2022-10-07 19:06:52 -0400

    yes

--- image_grabber.py
+++ image_grabber.py
@@ -8,21 +8,18 @@ import time
 import csv
 import os
 
-# TODO do google searches all in one go, change so we dont have to get to the search page over and over
-#      instead, we would just enter the next query in the already available search bar
-#      then... do the next search engine and repeat
-
 # TODO
 print("Bing search not implemented!")
 
-csv_file = 'florida_mushi.csv'
+csv_file_name = 'florida_mushi.csv'
 
-search_engine_max = 2  # google, duckduckgo, bing
+google = False
+duckduckgo = True
 
-smallest_csv_count = 25  # smallest amount of instances, last column in the csv file
+smallest_csv_count = 250  # smallest amount of instances, last column in the csv file; i.e. 25
 
-limit = 125  # number of images you may download
-smallest_allowed = 50  # smallest amount of images you're allowed to download w/o being useless
+limit = 5  # number of images you may download; i.e. 125
+smallest_allowed = 5  # smallest amount of images you're allowed to download w/o being useless; i.e. 50
 
 try:
     os.mkdir("images")
@@ -38,73 +35,45 @@ global_start_time = time.time()
 local_total_time_array = list()
 
 total_count = 0
+search_engine = 1
 
-with open(csv_file) as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
+if google:
+    with open(csv_file_name) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
 
-    for row in csv_reader:
+        search_engine_name = 'google'
+        print(f"\nUsing search engine: {search_engine_name}")
 
-        search_engine = 1
+        browser.get('https://www.google.com/')
+        image_link = browser.find_element(by=By.LINK_TEXT, value='Images')
+        image_link.get_attribute('href')
+        image_link.click()
 
-        if line_count == 0:
-            line_count += 1
-            continue
-        elif int(row[3]) < smallest_csv_count:
-            continue
-        else:
-            query = row[1] + ' ' + row[2]
-            print(f"\n\nQuery: {query}")
+        search = browser.find_element(by=By.NAME, value='q')
 
-            try:
-                os.mkdir("images/" + query)
-            except Exception as p:
-                print(p, "\nOverwriting")
+        for row in csv_reader:
 
-            local_start_time = time.time()
+            if line_count == 0:
+                line_count += 1
+                continue
+            elif int(row[3]) < smallest_csv_count:
+                continue
+            else:
+                query = row[1] + ' ' + row[2]
+                print(f"\n\nQuery: {query}")
 
-            while search_engine <= search_engine_max:
+                try:
+                    os.mkdir("images/" + query)
+                except Exception as p:
+                    print(p, "\nOverwriting")
 
-                search_engine_name = ''
-                if search_engine == 1:
-                    search_engine_name = 'google'
-                elif search_engine == 2:
-                    search_engine_name = 'duckduckgo'
-                elif search_engine == 3:
-                    search_engine_name = 'bing'
+                local_start_time = time.time()
 
-                print(f"\nUsing search engine: {search_engine_name}")
-
-                if search_engine == 1:
-                    browser.get('https://www.google.com/')
-
-                    image_link = browser.find_element(by=By.LINK_TEXT, value='Images')
-                    image_link.get_attribute('href')
-                    image_link.click()
-
-                    search = browser.find_element(by=By.NAME, value='q')
-                    query_pedantic = '\"' + query + '\"'
-                    search.send_keys(query_pedantic, Keys.ENTER)
-
-                elif search_engine == 2:
-                    browser.get('https://duckduckgo.com/?q=')
-
-                    search = browser.find_element(by=By.ID, value="search_form_input_homepage")
-                    search.get_attribute('href')
-                    search.click()
-
-                    query_pedantic = '\"' + query + '\"'
-                    search.send_keys(query_pedantic, Keys.ENTER)
-                    browser.implicitly_wait(2)
-
-                    image_link = browser.find_element(by=By.PARTIAL_LINK_TEXT, value="Images")
-                    image_link.get_attribute('href')
-                    image_link.click()
-
-                elif search_engine == 3:
-                    # TODO
-                    browser.get('bing.com')
-                    break
+                query_pedantic = '\"' + query + '\"'
+                search = browser.find_element(by=By.NAME, value='q')
+                search.clear()
+                search.send_keys(query_pedantic, Keys.ENTER)
 
                 value = 0
                 for i in range(10):
@@ -113,12 +82,8 @@ with open(csv_file) as csv_file:
                     time.sleep(0.1)
 
                 try:
-                    if search_engine == 1:
-                        screen_ = browser.find_element(by=By.ID, value="islmp")
-                        image_ = screen_.find_elements(by=By.TAG_NAME, value='img')
-                    elif search_engine == 2:
-                        screen_ = browser.find_element(by=By.ID, value="zero_click_wrapper")
-                        image_ = screen_.find_elements(by=By.TAG_NAME, value='img')
+                    screen_ = browser.find_element(by=By.ID, value="islmp")
+                    image_ = screen_.find_elements(by=By.TAG_NAME, value='img')
                 except Exception as p:
                     print(p, f"Failed at grabbing webpage on query: {query}")
                     search_engine += 1
@@ -168,12 +133,121 @@ with open(csv_file) as csv_file:
                 if count != 0:
                     total_count += count - 1
 
-                search_engine += 1
+                local_total_time_array.append(time.time() - local_start_time)
+                line_count += 1
+        print(f'\n\nProcessed {line_count} lines of the .csv file!')
+        csv_file.close()
 
-            local_total_time_array.append(time.time() - local_start_time)
-            # print(f"Finished in: {time.time() - local_start_time}")
-            line_count += 1
-    print(f'\n\nProcessed {line_count} lines of the .csv file!')
+if duckduckgo:
+    with open(csv_file_name) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+
+        browser.get('https://duckduckgo.com/?q=')
+
+        search = browser.find_element(by=By.ID, value="search_form_input_homepage")
+        search.get_attribute('href')
+        search.click()
+
+        search.send_keys(".", Keys.ENTER)
+        browser.implicitly_wait(2)
+
+        image_link = browser.find_element(by=By.PARTIAL_LINK_TEXT, value="Images")
+        image_link.get_attribute('href')
+        image_link.click()
+
+        for row in csv_reader:
+
+            if line_count == 0:
+                line_count += 1
+                continue
+            elif int(row[3]) < smallest_csv_count:
+                continue
+            else:
+                query = row[1] + ' ' + row[2]
+                print(f"\n\nQuery: {query}")
+
+                try:
+                    os.mkdir("images/" + query)
+                except Exception as p:
+                    print(p, "\nOverwriting")
+
+                local_start_time = time.time()
+
+                search_engine_name = 'duckduckgo'
+
+                print(f"\nUsing search engine: {search_engine_name}")
+
+                query_pedantic = '\"' + query + '\"'
+                search = browser.find_element(by=By.ID, value="search_form_input")
+                # TODO, this does not work on duckduckgo?
+                # search.clear()
+                search.send_keys(query_pedantic, Keys.ENTER)
+
+                value = 0
+                for i in range(10):
+                    browser.execute_script("scrollBy(" + str(value) + ",+1000);")
+                    value += 1000
+                    time.sleep(0.1)
+
+                try:
+                    screen_ = browser.find_element(by=By.ID, value="zero_click_wrapper")
+                    image_ = screen_.find_elements(by=By.TAG_NAME, value='img')
+                except Exception as p:
+                    print(p, f"Failed at grabbing webpage on query: {query}")
+                    search_engine += 1
+                    continue
+
+                current = 0
+                counter = 0
+
+                query_path = "images/" + query + '/' + search_engine_name
+
+                try:
+                    os.mkdir(query_path)
+                except Exception as p:
+                    print(p, f"Error making {query_path}")
+
+                count = 0
+                for i in image_:
+                    src = i.get_attribute('src')
+                    count += 1
+                    if count > limit:
+                        break
+                    if src:
+                        src = str(src)
+                        print("Downloaded image: ", count)
+                        try:
+                            urllib.request.urlretrieve(src,
+                                                       os.path.join(f'{query_path}', 'image' + str(count) + '.jpg'))
+                        except Exception as p:
+                            count -= 1
+                            print(p, f'Could not print image: {count}\n')
+                    else:
+                        count -= 1
+                        print(f'Could not print image: {count}\n')
+
+                try:
+                    if count < smallest_allowed:
+                        error_log.write(f"SMALL IMAGE COUNT, {query}\n")
+                        shutil.rmtree(query_path)
+                        print(f"Deleted useless query \"{query_path}\"")
+                        count = 0
+                    # else:
+                    #    print("Total images downloaded: ", (count - 1))
+                except Exception as p:
+                    print(p, f"\nFailed to delete useless folder \"{query_path}\"")
+                    count = 0
+
+                if count != 0:
+                    total_count += count - 1
+
+                local_total_time_array.append(time.time() - local_start_time)
+                # print(f"Finished in: {time.time() - local_start_time}")
+                line_count += 1
+        print(f'\n\nProcessed {line_count} lines of the .csv file!')
+
+    csv_file.close()
 
 avg_time = sum(local_total_time_array) / len(local_total_time_array)
 global_time = time.time() - global_start_time
